5d61c1b7782d2a4a1e9220b56ec3c93a5a99fbec
theepan <neotechmonk@gmail.com>
2018-10-01 22:31:02 +1000

    istanbul test coverage

--- test/appTest.js
+++ test/appTest.js
@@ -2,25 +2,26 @@
 
 const assert = require("chai").assert;
 const app = require("../app");
+const mocha = require("mocha");
 
 let getNameResult = app.getName();
 let addNumberResult = app.addNumber(5, 5);
 
-describe("App ", function() {
-    describe("App-GetName ", function() {
-      //Expected value
-      it("getName should return Theepan", function() {
-        assert.equal(getNameResult, "Theepan");
-      });
-      //No expected value
-      it("getName should not  return other Theepan", function() {
-        assert.notEqual(getNameResult, "Arunan");
-      });
-  
-      //Type of return value
-      it("getName should always return a string", function() {
-        assert.typeOf(getNameResult, "string");
-      });
+
+mocha.describe("App ", function() {
+  mocha.describe("App-GetName ", function() {
+    //Expected value
+    it("getName should return Theepan", function() {
+      assert.equal(getNameResult, "Theepan");
+    });
+    //No expected value
+    it("getName should not  return other Theepan", function() {
+      assert.notEqual(getNameResult, "Arunan");
+    });
+
+    //Type of return value
+    it("getName should always return a string", function() {
+      assert.typeOf(getNameResult, "string");
     });
   });
-  
+});
